a6d7e22f10c44b3d84a74d7cf5b7ec9cbbc002ad
daoduyquy321323 <daoduyquylop97@gmail.com>
2024-07-31 15:16:00 +0700

    error

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://deploy-be-mysql.up.railway.app',
+    baseURL: 'http://localhost:8081',
     withCredentials: true,
 });
 
